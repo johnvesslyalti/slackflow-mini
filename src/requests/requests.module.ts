3814acd9bbab44c93a5878fabb0a365ba3f53c97
johnvesslyalti <altijohnvessly@gmail.com>
2026-01-25 16:20:07 +0530

    feat: Implement Prisma PostgreSQL adapter, configure TypeScript for CommonJS, and enable RequestsService for SlackModule.

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 
 @Module({
-  providers: [RequestsService]
+  providers: [RequestsService],
+  exports: [RequestsService]
 })
 export class RequestsModule {}
--- src/slack/slack.module.ts
+++ src/slack/slack.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { SlackController } from './slack.controller';
 import { SlackService } from './slack.service';
 
+import { RequestsModule } from '../requests/requests.module';
+
 @Module({
+  imports: [RequestsModule],
   controllers: [SlackController],
   providers: [SlackService]
 })
