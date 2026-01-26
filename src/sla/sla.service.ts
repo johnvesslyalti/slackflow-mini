73c97186da4ced2ebeca0ebd2bdc49f2f4437ecd
johnvesslyalti <altijohnvessly@gmail.com>
2026-01-26 14:05:15 +0530

    feat: Implement SLA lifecycle management with start, pause, and resume functionalities, and integrate SLA creation with ticket generation.

--- src/sla/sla.service.ts
+++ src/sla/sla.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { SlaRepository } from './sla.repository';
+import { SLAStatus } from '@prisma/client';
 
 @Injectable()
-export class SlaService {}
+export class SlaService {
+    constructor(private slaRepository: SlaRepository) {}
+
+    start(ticketId: string) {
+        const DEFAULT_DURATION = 30 * 60
+        return this.slaRepository.createForTicket(ticketId, DEFAULT_DURATION)
+    }
+
+    async pause(ticketId: string) {
+        const sla = await this.slaRepository.findByTicketId(ticketId)
+
+        if (!sla || sla.status !== 'ACTIVE') return;
+        
+        await this.slaRepository.update(sla.id, {
+            status: SLAStatus.PAUSED,
+            pausedAt: new Date()
+        });
+    }
+
+    async resume(ticketId: string) {
+        const sla = await this.slaRepository.findByTicketId(ticketId)
+
+        if (!sla || sla.status !== 'PAUSED' || !sla.pausedAt) return;
+
+        const pausedDuration = (Date.now() - sla.pausedAt.getTime() / 1000);
+
+        await this.slaRepository.update(sla.id, {
+            status: SLAStatus.ACTIVE,
+            pausedAt: null,
+            totalPausedDuration: sla.totalPaused + Math.floor(pausedDuration)
+        })
+    }
+}
--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { TicketsRepository } from './tickets.repository';
+import { SlaService } from 'src/sla/sla.service';
 
 @Injectable()
-export class TicketsService {}
+export class TicketsService {
+    constructor(
+        private ticketRepository: TicketsRepository,
+        private slaService: SlaService
+    ) {}
+
+    async createFromRequest(requestId: string, agentId: string) {
+        const ticket = await this.ticketRepository.createFromRequest(requestId, agentId);
+
+        await this.slaService.start(ticket.id);
+
+        return ticket;
+    }   
+}
