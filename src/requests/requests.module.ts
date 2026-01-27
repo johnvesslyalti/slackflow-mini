e466889747caeeb6504163059d05fd68a7ede438
johnvesslyalti <altijohnvessly@gmail.com>
2026-01-27 20:18:01 +0530

    fix: remove unused SlaService import

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -3,7 +3,6 @@ import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
 import { TicketsModule } from 'src/tickets/tickets.module';
 import { RequestsRepository } from './requests.repository';
-import { SlaService } from 'src/sla/sla.service';
 
 @Module({
   imports: [TicketsModule],
